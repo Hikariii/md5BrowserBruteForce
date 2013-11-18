673b3191fcb9948a8535a3bffb9336f5ff8f0a64
Sebassie <sebassie@fake.com>
2013-11-18 17:00:21 +0100

    bruteforce with progress and time

--- bruteforce.py
+++ bruteforce.py
@@ -1,26 +1,37 @@
 # your code goes here
 import string
 import hashlib
+import time
+import datetime
 
 #define charset
 hashChars = string.ascii_lowercase + string.ascii_uppercase
-hashChars = ['a','b','c'] #for testing
 
 #hash to be found
 hashVal = "f92bfc75d481c196c1e4deecf7fac3af"
-hashVal = "9df62e693988eb4e1e1444ece0578579" #for testing
+counter = 0
+
+start_time = time.clock()
 
 #bruteForce for "a-zA-z" strings
 def bruteForce(s=''):
   maxLength = 4
-  if len(s) >= maxLength:
+  global counter
+  counter += 1
+  if len(s) > maxLength:
     return None
-  elif hashlib.md5(s).hexdigest() == hashVal:
+  elif hashlib.md5(s.encode('utf-8')).hexdigest() == hashVal:
     return s
+
+  #show progress
+  if (counter % 1000 == 0):
+    print(s)
   
   for c in hashChars:
     res = bruteForce(s+c)
     if (res is not None):
       return res
 
-print bruteForce()
+print(bruteForce())
+
+print("Elapsed (h.mm.ssssssss):" + str(datetime.timedelta(seconds=(time.clock() - start_time))))
